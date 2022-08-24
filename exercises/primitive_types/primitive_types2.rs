3f52ba3cd2f55d91c2fa3b87a64521d5d705fa32
Maciek <msitkowski94@gmail.com>
2022-08-24 22:23:59 +0200

    primitive types, part 1

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -2,7 +2,7 @@
 // Fill in the rest of the line that has code missing!
 // No hints, there's no tricks, just get used to typing these :)
 
-// I AM NOT DONE
+use std::any::Any;
 
 fn main() {
     // Characters (`char`)
@@ -18,14 +18,24 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let // Finish this line like the example! What's your favorite character?
-    // Try a letter, try a number, try a special character, try a character
-    // from a different language than your own, try an emoji!
+    let your_character = '1'; // Finish this line like the example! What's your favorite character?
+                              // Try a letter, try a number, try a special character, try a character
+                              // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
     } else if your_character.is_numeric() {
         println!("Numerical!");
     } else {
         println!("Neither alphabetic nor numeric!");
+    }
+
+    let char_number = '1';
+    if char_number.is_ascii_digit() {
+        println!("'1' is ascii digit");
+    }
+
+    let char_special = '!';
+    if char_special.is_ascii_punctuation() {
+        println!("'!' is punctuation");
     }
 }
