1c79afa3484d45b66a60e667d0e8fcceb130e944
Maciek Sitkowski <msitkowski94@gmail.com>
2025-01-22 08:31:17 +0100

    day 3: if + quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,11 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let cost = if quantity > 40 { 1 } else { 2 };
+
+    quantity * cost
+}
 
 fn main() {
     // You can optionally experiment here.
