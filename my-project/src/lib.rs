9d48b5125156d0259cfd9831cb0cfd62522b19d8
Maciek Sitkowski <msitkowski94@gmail.com>
2025-03-23 06:48:34 +0100

    day 27: module paths and use keyword

--- my-project/src/lib.rs
+++ my-project/src/lib.rs
@@ -6,6 +6,8 @@ pub mod front_of_house {
     }
 
     mod serving {
+        use rand::Rng;
+
         fn take_order() {}
 
         fn serve_order() {}
@@ -14,10 +16,83 @@ pub mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub enum Appetizer {
+        Salad,
+        Soup,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+
+        pub fn start_preparing(&self) {
+            println!(
+                "Oye! Toast with {} bread and {}!!!",
+                self.toast, self.seasonal_fruit
+            )
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+fn deliver_order() {}
+
+// Bringing hosting to scope using `use` keyword
+use crate::front_of_house::hosting;
+
+mod customer {
+    // above `use` is no longer used in this scope so with need to
+    // add here as well
+    use crate::front_of_house::hosting;
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
+}
+
 pub fn eat_at_restaurant() {
+    // Paths
+    //////////////////////////////////////////////////////
+
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // hosting is in scope because we brought it in with `use` above
+    hosting::add_to_waitlist();
+
+    // Public and private Structs and Enums
+    //////////////////////////////////////////////////////
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Changed mind about bread type
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    back_of_house::Breakfast::start_preparing(&meal);
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,8 +1,28 @@
-use crate::garden::vegetables::Asparagus;
-
-pub mod garden;
+use rand::Rng;
+use std::io;
 
 fn main() {
-    let plant = Asparagus {};
-    println!("I'm growing {plant:?}!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=2);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    if guess == secret_number {
+        println!("Correct: {guess} is a secret number!");
+    } else {
+        println!("Wrong: Secret number is {secret_number}!");
+    }
 }
