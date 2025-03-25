ac1a74d9e704a7a93f403ab78eca0c33b871d90d
Maciek Sitkowski <msitkowski94@gmail.com>
2025-03-25 08:36:54 +0100

    day 29: hash maps

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -8,18 +8,48 @@ use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
-    // let mut basket =
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket.
+    basket.insert(String::from("apple"), 10);
+    basket.insert(String::from("peach"), 1);
+    basket.insert(String::from("watermelon"), 2);
+    basket.insert(String::from("strawberry"), 19);
 
     basket
 }
 
 fn main() {
-    // You can optionally experiment here.
+    // # Hash Maps and Ownership
+    ////////////////////////////////////////////////////////
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+    // println!("{}", field_name);
+
+    // # Updating a Hash Map
+    ////////////////////////////////////////////////////////
+    // ## Overwriting a Value
+    let mut scores1 = HashMap::new();
+    scores1.insert(String::from("Blue"), 10);
+    scores1.insert(String::from("Blue"), 25);
+    println!("[scores1] Overwriting: {scores1:?}");
+
+    // ## Adding a Key and Value Only If a Key Isn’t Present
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("Blue"), 10);
+    scores2.entry(String::from("Yellow")).or_insert(50);
+    scores2.entry(String::from("Blue")).or_insert(50);
+    println!("[scores2] Adding if not present: {scores2:?}");
+
+    // ## Updating a Value Based on the Old Value
 }
 
 #[cfg(test)]
