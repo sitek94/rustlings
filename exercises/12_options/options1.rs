cc5ce6af5f7a4ce65e72a1e1c1d8feb3e1c0189b
Maciek Sitkowski <msitkowski94@gmail.com>
2025-04-01 11:14:31 +0200

    day 32: options and range pattern

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -3,7 +3,25 @@
 // someone eats it all, so no icecream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    // TODO: Complete the function body
+    // My first take. I went with if-else but was wondering
+    // if it's possible to do this using `match`
+    // if hour_of_day < 22 {
+    //     Some(5)
+    // } else if hour_of_day <= 23 {
+    //     Some(0)
+    // } else {
+    //     None
+    // }
+
+    // Turns out, it's possible!
+    match hour_of_day {
+        // This is called `range pattern` with an inclusive upper bound
+        // `..=` includes both 0 and 21. Just `0..21` would exclude 21
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        _ => None,
+    }
 }
 
 fn main() {
@@ -18,7 +36,8 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        // Alternatively: `expect` and other variants of `unwrap_or` etc
+        let icecreams = maybe_icecream(12).unwrap();
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
