6dcf2fed4c1df52e65fab7320b30b1dd2df2ea7d
Maciek Sitkowski <msitkowski94@gmail.com>
2025-04-28 08:41:04 +0200

    day 36: match and references

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -8,8 +8,17 @@ fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
     // TODO: Fix the compiler error by adding something to this match statement.
-    match optional_point {
+
+    // 1. Matching over reference by adding `&`
+    match &optional_point {
         Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        _ => panic!("No match!"),
+    }
+
+    // Alternatively:
+    // 2. Match over Option without moving out of the Some variant
+    match optional_point {
+        Some(ref p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
 
